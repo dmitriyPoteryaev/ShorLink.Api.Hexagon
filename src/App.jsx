28b86e66decf732a4f64ff1be08612115fa6ce75
dmitriyPoteryaev <dim.poteryaeff@yandex.ru>
2022-09-01 22:12:04 +0300

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import React from "react";
+import Greeting_Validator from "./pages/Content/Content";
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import Greeting from "./pages/Greeting/Greeting";
+import Navbar from "./components/UI/Navbar/Navbar";
 import "../style/style.css";
 
 
 function App() {
     return (
 
-<div></div>
-
+<div className="App">
+      <BrowserRouter>
+      {/* <Navbar/> */}
+        <Routes>
+          <Route path="/Greeting" element={<Greeting_Validator />} />
+          <Route path="*" element={<Greeting_Validator />} />
+        </Routes>
+      </BrowserRouter>
+      </div>
 
     );}
 
